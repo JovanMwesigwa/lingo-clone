41b5303d85f0687647e1da1e0844dc6dd81502a2
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2024-03-16 19:33:10 +0300

    Remove unused Button component from Home page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Header } from "./Header";
+import { Footer } from "./footer";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col ">
+      <Header />
+      <main className="flex flex-1 flex-col items-center justify-center">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MarketingLayout;
