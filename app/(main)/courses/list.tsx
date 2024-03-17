47256a1bf34f2939a0696d5f8ef0234481761b67
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2024-03-17 11:13:41 +0300

    Update courses list and user progress handling

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,14 +1,37 @@
 "use client";
 
-import { courses } from "@/db/schema";
+import { useTransition } from "react";
+
+import { courses, userProgress } from "@/db/schema";
 import Card from "./card";
+import { redirect } from "next/navigation";
+import { upsertUserProgress } from "@/actions/user-progress";
+import { toast } from "sonner";
 
 type Props = {
   courses: (typeof courses.$inferSelect)[];
-  activeCourse: number;
+  activeCourse?: typeof userProgress.$inferSelect.activeCourseId;
 };
 
 const List = ({ activeCourse, courses }: Props) => {
+  const [pending, startTransition] = useTransition();
+
+  const onClick = (courseId: number) => {
+    if (pending) {
+      return;
+    }
+
+    if (activeCourse === courseId) {
+      redirect("/learn");
+    }
+
+    startTransition(() => {
+      upsertUserProgress(courseId).catch(() =>
+        toast.error("Something went wrong")
+      );
+    });
+  };
+
   return (
     <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
       {courses.map((course) => (
@@ -18,8 +41,8 @@ const List = ({ activeCourse, courses }: Props) => {
           active={course.id === activeCourse}
           title={course.title}
           imageSrc={course.imageSrc}
-          disabled={false}
-          onClick={() => {}}
+          disabled={pending}
+          onClick={onClick}
         />
       ))}
     </div>
